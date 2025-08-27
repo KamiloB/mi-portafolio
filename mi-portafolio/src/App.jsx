4b9940f814c784294955a72ae70e4879b353a97e
Kamilo Blandon <kamilob1224@gmail.com>
2025-08-27 00:14:14 -0500

    Corregir mayúsculas en imports para Linux

--- mi-portafolio/src/App.jsx
+++ mi-portafolio/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header";
+import Header from "./components/Header";
 import About from "./components/about";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
